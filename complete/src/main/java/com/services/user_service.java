63a7c5a1a2240a95f54dcce519e4084179c738f8
webdev219 <aleksaggcweb@gmail.com>
2025-01-02 10:39:48 -0500

    add security

---
+++ complete/src/main/java/com/services/user_service.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot.service;
+
+import net.javaguides.springboot.dto.UserDto;
+import net.javaguides.springboot.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    void saveUser(UserDto userDto);
+
+    User findUserByEmail(String email);
+
+    List<UserDto> findAllUsers();
+}
